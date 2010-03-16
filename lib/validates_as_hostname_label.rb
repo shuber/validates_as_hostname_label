439daaf82d687eb235e4e1d58e81e0bce7f5f0dc
Sean Huber <shuber@huberry.com>
2010-03-15 17:01:36 -0700

    refactor

--- lib/validates_as_hostname_label.rb
+++ lib/validates_as_hostname_label.rb
@@ -1,37 +1,58 @@
+# Adds ActiveRecord validation for hostname labels
 module ValidatesAsHostnameLabel
 
-  RESERVED_HOSTNAMES = %w(www blog dev stage stats status admin ftp sftp mail pop pop3 imap smtp)
+  # A hash of default options to use when calling <tt>validates_as_hostname_label</tt>.
+  #
+  # Defaults:
+  #
+  #   :allow_underscores  =>  false
+  #   :reserved           =>  %w(admin blog dev development ftp help imap mail pop pop3 sftp smtp staging stats status support www)
+  def self.default_options
+    @default_options ||= {
+      :allow_underscores => false,
+      :reserved => %w(admin blog dev development ftp help imap mail pop pop3 sftp smtp staging stats status support www)
+    }
+  end
   
-  # Checks for:
+  # Validates hostname labels by checking for:
   #
   #   * Length between 1 and 63 characters long
   #   * Letters 'a' through 'z' (case-insensitive), the digits '0' through '9', and the hyphen (and optionally the underscore)
   #   * Labels that don't begin or end with a hyphen or underscore
-  #   * Reserved labels
+  #   * Reserved hostname labels
   #
-  # Accepts an :allow_underscores option which defaults to false
+  # Options:
+  #
+  #   :allow_underscores  -  Allows underscores in hostname labels. Defaults to false.
+  #   :reserved           -  Contains an array of reserved hostname labels to validate exclusion of. 
+  #                          Defaults to ValidatesAsHostnameLabel.default_options[:reserved]
   def validates_as_hostname_label(*attrs)
-    options = { :allow_underscores => false, :reserved => RESERVED_HOSTNAMES }.merge(attrs.last.is_a?(Hash) ? attrs.pop : {})
-    
-    format = 'a-z0-9\-'
-    format << '_' if options.delete(:allow_underscores)
-    
-    validates_length_of *attrs + [{
-      :in => 1..63,
-      :message => I18n.t('validates_as_hostname_label.invalid_length', :default => 'must be between 1 and 63 characters long')
-    }.merge(options)]
+    options = ValidatesAsHostnameLabel.default_options.merge(attrs.extract_options!)
 
-    validates_exclusion_of *attrs + [{
-      :in => options.delete(:reserved),
-      :message => I18n.t('validates_as_hostname_label.reserved', :default => 'is reserved')
-    }.merge(options)]
+    format, characters = 'a-z0-9\-', %w(alphanumeric hyphen)
+    format << '_' and characters << 'underscore' if options.delete(:allow_underscores)
 
-    validates_format_of *attrs + [{ :with => /^[#{format}]*$/i }.merge(options)]
+    I18n.with_options :scope => 'validates_as_hostname_label' do |i18n|
+      validates_exclusion_of *attrs + [{
+        :in => options.delete(:reserved),
+        :message => i18n.t('reserved', :default => 'is reserved')
+      }.merge(options)]
 
-    validates_format_of *attrs + [{
-      :with => /^[^-_].*[^-_]$/i,
-      :message => I18n.t('validates_as_hostname_label.invalid_first_character', :default => "can't start or end with a hyphen or underscore")
-    }.merge(options)]
+      validates_format_of *attrs + [{ 
+        :with => /^[#{format}]*$/i,
+        :message => i18n.t('invalid_format', :default => "may only contain #{characters.to_sentence} characters")
+      }.merge(options)]
+
+      validates_format_of *attrs + [{
+        :with => /^[^-_].*[^-_]$/i,
+        :message => i18n.t('invalid_prefix_or_suffix', :default => "may not start or end with a hyphen or underscore")
+      }.merge(options)]
+
+      validates_length_of *attrs + [{
+        :in => 1..63,
+        :message => i18n.t('invalid_length', :default => 'must be between 1 and 63 characters long')
+      }.merge(options)]
+    end
   end
 end
 
--- test/validates_as_hostname_label_test.rb
+++ test/validates_as_hostname_label_test.rb
@@ -1,5 +1,4 @@
 require 'rubygems'
-gem 'activerecord'
 require 'active_record'
 require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/validates_as_hostname_label'
@@ -15,7 +14,7 @@ class Account < ActiveRecord::Base
 end
 
 class ValidatesAsHostnameLabelTest < Test::Unit::TestCase
-  
+
   def setup
     ActiveRecord::Base.connection.tables.each { |table| ActiveRecord::Base.connection.drop_table(table) }
     silence_stream(STDOUT) do
@@ -30,49 +29,49 @@ class ValidatesAsHostnameLabelTest < Test::Unit::TestCase
       end
     end
   end
-  
+
   def test_should_save_with_valid_subdomains
     @account = Account.new :subdomain => 'testing', :subdomain_with_underscores => 'testing', :subdomain_with_nil => 'testing', :subdomain_with_blank => 'testing', :subdomain_with_reserved => 'testing'
     assert @account.save
   end
-  
+
   def test_should_save_subdomains_with_hyphens
     @account = Account.new :subdomain => 'test-ing', :subdomain_with_underscores => 'test-ing', :subdomain_with_nil => 'test-ing', :subdomain_with_blank => 'test-ing', :subdomain_with_reserved => 'test-ing'
     assert @account.save
   end
-  
+
   def test_should_not_save_with_blank_subdomain_if_allow_option_is_not_specified
     @account = Account.new :subdomain => nil
     assert !@account.save
     assert @account.errors.on(:subdomain)
-    
+
     @account.subdomain = ''
     assert !@account.save
     assert @account.errors.on(:subdomain)
   end
-  
+
   def test_should_save_with_blank_subdomain
     @account = Account.new :subdomain => 'testing', :subdomain_with_underscores => 'testing', :subdomain_with_nil => 'testing', :subdomain_with_blank => '', :subdomain_with_reserved => 'testing'
     assert @account.save
   end
-  
+
   def test_should_save_with_nil_subdomain
     @account = Account.new :subdomain => 'testing', :subdomain_with_underscores => 'testing', :subdomain_with_nil => nil, :subdomain_with_blank => 'testing', :subdomain_with_reserved => 'testing'
     assert @account.save
   end
-  
+
   def test_should_not_save_with_too_long_of_a_subdomain
     @account = Account.new :subdomain => ('t' * 64)
     assert !@account.save
     assert @account.errors.on(:subdomain)
   end
-  
+
   def test_should_not_save_with_invalid_characters
     @account = Account.new :subdomain => '!@#$%^&*'
     assert !@account.save
     assert @account.errors.on(:subdomain)
   end
-  
+
   def test_should_not_save_with_subdomains_beginning_with_a_hyphen_or_underscore
     @account = Account.new :subdomain => '-testing'
     @account.subdomain_with_underscores = '_testing'
@@ -80,7 +79,7 @@ class ValidatesAsHostnameLabelTest < Test::Unit::TestCase
     assert @account.errors.on(:subdomain)
     assert @account.errors.on(:subdomain_with_underscores)
   end
-  
+
   def test_should_not_save_with_subdomains_ending_with_a_hyphen_or_underscore
     @account = Account.new :subdomain => 'testing-'
     @account.subdomain_with_underscores = 'testing_'
@@ -88,20 +87,20 @@ class ValidatesAsHostnameLabelTest < Test::Unit::TestCase
     assert @account.errors.on(:subdomain)
     assert @account.errors.on(:subdomain_with_underscores)
   end
-  
+
   def test_should_not_save_subdomains_with_an_underscore_if_allow_underscores_option_is_false
     @account = Account.new :subdomain => 'test_ing'
     assert !@account.save
     assert @account.errors.on(:subdomain)
   end
-  
+
   def test_should_save_subdomains_with_an_underscore_if_allow_underscores_option_is_true
     @account = Account.new :subdomain => 'testing', :subdomain_with_underscores => 'test_ing', :subdomain_with_nil => 'testing', :subdomain_with_blank => 'testing', :subdomain_with_reserved => 'testing'
     assert @account.save
   end
-  
+
   def test_should_not_save_with_a_reserved_subdomain_from_the_default_list
-    ValidatesAsHostnameLabel::RESERVED_HOSTNAMES.each do |hostname|
+    ValidatesAsHostnameLabel::default_options[:reserved].each do |hostname|
       @account = Account.new :subdomain => hostname
       assert !@account.save
       assert @account.errors.on(:subdomain)
@@ -113,11 +112,11 @@ class ValidatesAsHostnameLabelTest < Test::Unit::TestCase
     assert !@account.save
     assert @account.errors.on(:subdomain_with_reserved)
   end
-  
+
   def test_should_not_be_valid_with_a_subdomain_not_from_the_list
     @account = Account.new :subdomain_with_reserved => 'qqqfds'
     assert !@account.save
     assert !@account.errors.on(:subdomain_with_reserved)
   end
-  
+
 end
